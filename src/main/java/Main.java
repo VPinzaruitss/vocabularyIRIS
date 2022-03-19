5400d47e84274ea80853b9479ed436599a51e00b
KGrechka <KGrechka@itssglobal.com>
2022-03-19 19:10:49 +0200

    ver 1.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,59 +1,42 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Options options = getCommandOptions();
 
-    private static ObjectMapper objectMapper = new ObjectMapper();
+        CommandLine cmd;
+        try {
+            cmd = new DefaultParser().parse(options, args);
 
-    public static Options getCommandOptions() {
+            Path out = Paths.get(cmd.getOptionValue("o"));
+            Path src = Paths.get(cmd.getOptionValue("s"));
+
+            if (!Files.exists(src))
+                throw new FileNotFoundException();
+
+            Vocabulary deserializedFile = VocabularyService.deserializationFromJson(src);
+            VocabularyService.serializationIntoJson(deserializedFile, out);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.err.println("Parsing failed");
+        }
+    }
+
+    private static Options getCommandOptions() {
         // create Options object
         Options options = new Options();
 
-        options.addRequiredOption("h", "home", true, "TAFJ home path");
-        options.addOption("c", "config", true, "TAFJ configuration name");
         options.addRequiredOption("s", "src", true, "Source folder");
         options.addRequiredOption("o", "out", true, "Output folder");
-        options.addOption("i", "inserts", true, "Folder with additional inserts");
+
         return options;
-    }
-
-
-
-    public static void main(String[] args) {
-        serialization(deserialization(),null);
-
-    }
-
-    public static void serialization(Vocabulary vocabulary , Path path) {
-        try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
-            objectMapper.writeValue(writer, vocabulary);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static Vocabulary deserialization() {
-        Vocabulary vocabulary = null;
-        try (BufferedReader reader = Files.newBufferedReader(Paths.get("src\\main\\resources\\vocabulary-retail.json"), StandardCharsets.UTF_8)) {
-//            objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-            vocabulary = objectMapper.readValue(reader, Vocabulary.class);
-            System.out.println(vocabulary);
-
-//            for(Vocabulary.Entries entries: vocabulary.getEntries()){
-//                System.out.println(entries);
-//            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return vocabulary;
-
     }
 }
---
+++ src/main/java/VocabularyService.java
@@ -0,0 +1,35 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.NonNull;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class VocabularyService {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    // path - output file
+    public static void serializationIntoJson(Vocabulary vocabulary, @NonNull Path path) {
+        try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
+            objectMapper.writeValue(writer, vocabulary);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // path - file with JSON
+    public static Vocabulary deserializationFromJson(@NonNull Path path) {
+        Vocabulary vocabulary = null;
+
+        try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+            vocabulary = objectMapper.readValue(reader, Vocabulary.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return vocabulary;
+    }
+}
