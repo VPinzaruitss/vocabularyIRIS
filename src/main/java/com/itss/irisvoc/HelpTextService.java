b791699fdf03b74a4e8339ba7904256cb83050c6
KGrechka <KGrechka@itssglobal.com>
2022-04-12 14:41:50 +0300

    updated (get content from zip)

--- src/main/java/com/itss/irisvoc/HelpTextService.java
+++ src/main/java/com/itss/irisvoc/HelpTextService.java
@@ -2,35 +2,63 @@ package com.itss.irisvoc;
 
 import lombok.SneakyThrows;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class HelpTextService {
-    private static final Map<String, HelpText> elements = new HashMap<>();
+    private static final Map<String, String> elements = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
 
-        String fileName = "C:\\Users\\Kirill\\Desktop\\HELP.TEXT";
+        // todo add path as a parameter
 
-        Files.walk(Paths.get(fileName))
-                .filter(Files::isRegularFile)
-                .filter(path -> path.toString().toLowerCase().endsWith("xml"))
-                .forEach(HelpTextService::parse);
+        Path fileName = Paths.get("C:\\Users\\Kirill\\Desktop\\vocabularyIRIS\\Transact.HelpText.R21.zip");
 
-//        parse(Paths.get("C:\\Users\\Kirill\\Desktop\\test.xml"));
+        try (ZipInputStream zip = new ZipInputStream(Files.newInputStream(fileName))) {
+            ZipEntry entry;
 
-        for (Map.Entry<String, HelpText> entry : elements.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue().getDesc());
+            while ((entry = zip.getNextEntry()) != null) {
+
+                if (entry.isDirectory()) {
+                    continue;
+                }
+
+                if (entry.getName().toLowerCase().endsWith(".xml")) {
+
+                    try {
+
+                        String fileContent = readAll(zip);
+
+                        int startIndexFileName = entry.getName().lastIndexOf('/') + 1;
+
+                        // table = file name
+                        String table = entry.getName().substring(startIndexFileName, entry.getName().length() - 4); // remove '.xml'
+
+                        parse(fileContent, table);
+
+                    } catch (Exception ex) {
+                        System.err.println(entry.getName() + " [" + ex + "]");
+                    }
+                }
+            }
+        }
+
+        for (Map.Entry<String, String> entry : elements.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
         }
     }
 
     @SneakyThrows
-    private static void parse(Path path) {
-        String fileContent = String.join("", Files.readAllLines(path));
+    private static void parse(String fileContent, String table) {
         String[] splitFileByFieldTag = fileContent.split("<field>");
 
         for (String str : splitFileByFieldTag) {
@@ -38,15 +66,10 @@ public class HelpTextService {
             String desc = getDesc(str);
 
             if (!field.isEmpty()) {
-                HelpText helpText = new HelpText(field, desc);
+                // key = (file name (table) + field)
+                String key = table + "*" + field;
 
-                // key = (file name + field)
-                String key = path
-                        .getFileName()
-                        .toString()
-                        .substring(0, path.getFileName().toString().length() - 4) + "*" + field;
-
-                elements.put(key, helpText);
+                elements.put(key, desc);
             }
         }
     }
@@ -54,13 +77,13 @@ public class HelpTextService {
     private static String getField(String str) {
         return str.substring(0, str.indexOf('<'))
                 .replaceAll("<.*?>|\n|\t|\\s([\\s])+", "")
-                .replaceAll("&lt;", "<")
-                .replaceAll("&gt;", ">")
-                .replaceAll("&amp;", "&");
+                .replace("&lt;", "<")
+                .replace("&gt;", ">")
+                .replace("&amp;", "&");
     }
 
     private static String getDesc(String str) {
-        int startIndex= str.indexOf("<desc>");
+        int startIndex = str.indexOf("<desc>");
         int endIndex = str.indexOf("</desc>");
 
         String descContent;
@@ -79,8 +102,25 @@ public class HelpTextService {
 
         return descContent
                 .replaceAll("<.*?>|\n|\t|\\s([\\s])+", "")
-                .replaceAll("&lt;", "<")
-                .replaceAll("&gt;", ">")
-                .replaceAll("&amp;", "&");
+                .replace("&lt;", "<")
+                .replace("&gt;", ">")
+                .replace("&amp;", "&");
+    }
+
+    @SneakyThrows
+    public static String readAll(InputStream is) {
+        if (is == null) {
+            return "";
+        }
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        byte[] buffer = new byte[512];
+        int b;
+        while ((b = is.read(buffer)) >= 0) {
+            baos.write(buffer, 0, b);
+        }
+
+        return new String(baos.toByteArray(), StandardCharsets.UTF_8);
     }
 }
